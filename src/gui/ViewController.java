789a0b2ecc16136dbcb7b9419b0d2d3504e2c1af
Samuel Maciel <samuelmsilva@outlook.com.br>
2023-12-07 17:27:03 -0300

    Limitações para TextField e Initializable

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,28 +1,32 @@
 package gui;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 
-public class ViewController {
+public class ViewController implements Initializable {
 
 	@FXML
 	private TextField txtNumber1;
-	
+
 	@FXML
 	private TextField txtNumber2;
-	
+
 	@FXML
 	private Label labelResult;
-	
+
 	@FXML
 	private Button btSum;
-	
+
 	@FXML
 	public void onBtSumClick() {
 		try {
@@ -31,9 +35,16 @@ public class ViewController {
 			double width = Double.parseDouble(txtNumber2.getText());
 			double sum = height + width;
 			labelResult.setText(String.format("%.2f", sum));
-		}
-		catch (NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Constraints.setTextFieldDouble(txtNumber1);
+		Constraints.setTextFieldMaxLength(txtNumber1, 14);
+		Constraints.setTextFieldDouble(txtNumber2);
+		Constraints.setTextFieldMaxLength(txtNumber2, 14);
 	}
 }
